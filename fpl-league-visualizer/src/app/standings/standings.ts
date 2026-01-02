afcd76c4a02b8cd744306d3b9e08199974288a52
Naman Alagh <namanalagh19@gmail.com>
2026-01-03 01:05:31 +0530

    Chart params and Firebase setup

--- fpl-league-visualizer/src/app/standings/standings.ts
+++ fpl-league-visualizer/src/app/standings/standings.ts
@@ -31,6 +31,46 @@ export class Standings {
   loading = false;
   error = '';
   currentGw!: EventsDto;
+  _chartStartGw = 1;
+  _chartEndGw = this.currentGw?.id ?? 38
+
+  get chartStartGw(): number{
+    return this._chartStartGw;
+  }
+  set chartStartGw(value: number){
+    value = Math.floor(Number(value));
+
+    if (isNaN(value)) return;
+
+    // clamp
+    value = Math.max(1, value);
+    value = Math.min(value, this._chartEndGw-1);
+
+    this._chartStartGw = value;
+  }
+
+  get chartEndGw(): number{
+    return this._chartEndGw;
+  }
+  set chartEndGw(value: number) {
+    value = Math.floor(Number(value));
+
+    if (isNaN(value)) return;
+
+    // clamp
+    value = Math.min(this.currentGw.id, value);
+    value = Math.max(value, this._chartStartGw+1);
+
+    this._chartEndGw = value;
+  }
+
+  onStartGwChange(value: number) {
+    this.chartStartGw = value;
+  }
+
+  onEndGwChange(value: number) {
+    this.chartEndGw = value;
+  }
 
   get avgPts(): number {
     return this.totalPts/this.results.length
@@ -41,19 +81,19 @@ export class Standings {
 
   constructor(private standingsService: StandingsService, private playersService: PlayersService, private cdr: ChangeDetectorRef) {}
   
-  renderChart(uptoGw: number = 38) {
+  renderChart(fromGw: number = 1, uptoGw: number = 38) {
     if (!this.leagueChart) return;
     const top10Teams = this.standings.teams.slice(0, 10);
 
     const labels = Array.from(
-      { length: uptoGw },
-      (_, i) => `GW ${i + 1}`
+      { length: uptoGw - fromGw + 1 },
+      (_, i) => `GW ${i + fromGw}`
     );
 
     const datasets = top10Teams.map(team => ({
       label: team.entry_name,
       data: team.squad_by_gw
-        .slice(1, uptoGw + 1)
+        .slice(fromGw, uptoGw + 1)
         .map(s => this.getStatValue(s)),
       tension: 0.25,
       pointRadius: 0
@@ -104,12 +144,12 @@ export class Standings {
     this.results.forEach(res => {
       //this.getPicks(res.id);
       this.totalPts += res.total;
-      this.totalValue += res.squad_by_gw[19].value;
+      this.totalValue += res.squad_by_gw[this.currentGw.id].value;
       this.gwAvg += res.event_total;
     });
     
     this.gwAvg /= this.results.length;
-    this.renderChart(19);
+    this.renderChart(this.chartStartGw, this.chartEndGw);
   }
 
   private getStatValue(squad: Squad): number {
@@ -157,6 +197,7 @@ export class Standings {
     this.playersService.getStaticData().subscribe(data => {
       this.staticData = data
       this.currentGw = this.staticData.events.find(gw => gw.is_current)!
+      this.chartEndGw = this.currentGw?.id ?? 38
     });
   }
 }
